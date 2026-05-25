3c8f0fea6a1f6f4ca8ab5ec479ed8a990a57a555
elphie-pro <onazjr2006@gmail.com>
2026-05-25 23:18:31 +0100

    1024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
                 {/* Navbar */}
                 <div className="hidden md:block">
                   <ul className="flex gap-5 font-bold text-xl text-white cursor-pointer items-center">
-                      <li className="w-full bg-[#BE4A4A] p-1 justify-center flex rounded-xl md:text-sm lg:text-lg">
+                      <li className="px-2 p-1 justify-center flex rounded-xl md:text-sm lg:text-lg">
                       <Link href="/">Collection</Link>
                     </li>
                     <li className="md:text-sm lg:text-lg">
@@ -25,20 +25,20 @@ export default function Page() {
                 </div>
                 {/* logo */}
                 <div>
-                  <Image src="/logo.svg" alt="" width={20} height={20} className="w-full mt-[-5rem]"/>
+                  <Image src="/logo.svg" alt="" width={20} height={20} className="xl:w-full lg:w-[25rem] mt-[-5rem]"/>
                 </div>
                 {/* write up */}
                 <div className="flex flex-col gap-14">
-                    <div className="text-xl">
-                      <div className="absolute text-lg mt-[-9rem]">
+                    <div className="">
+                      <div className="absolute xl:text-lg xl:mt-[-9rem] lg:text-sm lg:mt-[-5rem]">
                         <h2>Empowering and giving the spotlight to creatives <br />
-                        <span className="ml-5">who don’t always get the recognition they deserve</span></h2>
+                        <span className="ml-2">who don’t always get the recognition they deserve</span></h2>
                       </div>
                     </div>
-                    <div className="text-right text-lg ">
-                      <div className="absolute mt-[-7rem]">
+                    <div className="text-right xl:text-lg lg:text-sm">
+                      <div className="absolute xl:mt-[-7rem] lg:mt-[-4.5rem]">
                           <h2>Underated exists for the artists, designers, photographers, <br />
-                           <span className="ml-[5rem]">filmmakers, digital creators and creatives in general</span>    <br />
+                           <span className="ml-[3rem]">filmmakers, digital creators and creatives in general</span>    <br />
                                                       <span className="ml-5">whose talents are real but overlooked.</span>         </h2>
                       </div>
                       
@@ -54,7 +54,7 @@ export default function Page() {
               <div className="flex flex-col gap-[17rem]">
               <div className="hidden md:block">
                   <ul className="flex gap-2 font-extrabold xl:text-xl lg:text-lg md:text-sm text-white cursor-pointer items-center">
-                      <li className="xl:w-full h-9 bg-[#BE4A4A] px-2 justify-center align-center items-center flex rounded-xl">
+                      <li className="p-1 h-9 bg-[#BE4A4A] px-2 justify-center align-center items-center flex rounded-xl">
                       <Link href="/">underated.mag</Link>
                     </li>
                     <button className="ml-[rem]">
@@ -62,11 +62,11 @@ export default function Page() {
                     </button>
                   </ul>
                 </div>
-                <div className="flex gap-10 justify-center items-center  xl:ml-[-8rem ml-[0]]">
-                    <div className="xl:w-[10rem] xl:h-[10rem] md:w-[5rem] md:h-[5rem]  bg-[#A11A1A] rounded-2xl">
+                <div className="flex gap-10 justify-center items-center xl:mt-0 lg:mt-[-7rem] xl:ml-[-8rem] lg:ml-[-5rem]">
+                    <div className="xl:w-[10rem] xl:h-[10rem] md:w-[5rem] md:h-[5rem] lg:w-[8.5rem] lg:h-[8.5rem] bg-[#A11A1A] rounded-2xl">
 
                     </div>
-                    <div className="xl:w-[10rem] xl:h-[10rem] md:w-[5rem] md:h-[5rem] bg-[#000] border-5 border-white rounded-2xl">
+                    <div className="xl:w-[10rem] xl:h-[10rem] md:w-[5rem] md:h-[5rem] lg:w-[8.5rem] lg:h-[8.5rem] bg-[#000] border-5 border-white rounded-2xl">
 
                     </div>
                 </div>
